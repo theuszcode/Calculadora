dd125f6eb63ba1f9c6b497c9016e4dc88348fadd
Matheus Ferreira <theuszdev@gmail.com>
2026-04-07 16:47:43 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,17 +1,16 @@
-# Calculator 
+# Calculadora
 num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
-num3 = int(input("Digite o terceiro número: "))
 operacao = input("Digite a operação: ")
 
 match operacao:
     case "+":
-        res = num1+num2+num3
+        res = num1+num2
     case "-": 
-        res = num1-num2-num3
+        res = num1-num2
     case "*":
-        res = num1*num2*num3
+        res = num1*num2
     case "/": 
-        res = num1/num2/num3
+        res = num1/num2
         
 print(f"resultado igual a {res} ")
